615c7ce63fac1141fd6a78b0d59f63e55ca7b206
arashhaji <56742302+arashhaji@users.noreply.github.com>
2020-02-21 12:13:49 -0500

    testing routers
    routers and model complete

---
+++ resources/resources-model.js
@@ -0,0 +1,24 @@
+const db = require('../data/db-config');
+
+module.exports = {
+  findAll,
+  findById,
+  add
+};
+
+function findAll() {
+  return db
+        ('resources');
+}
+
+function findById(id) {
+  return db
+         ('resources')
+         .where({ id });
+}
+
+async function add(data) {
+  return db
+        ('resources')
+        .insert(data);
+}
--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const server = express();
 
-// const ProjectRouter = require('./projects/projects-router');
-// const ResourcesRouter = require('./resources/resources-router');
-// const TasksRouter = require('./tasks/tasks-router');
+const ProjectRouter = require('./projects/projects-router');
+const ResourcesRouter = require('./resources/resources-router');
+const TasksRouter = require('./tasks/tasks-router');
 
 server.use(express.json());
 
-// server.use("/projects", ProjectRouter);
-// server.use("/resources", ResourcesRouter);
-// server.use("/tasks", TasksRouter);
+server.use("/projects", ProjectRouter);
+server.use("/resources", ResourcesRouter);
+server.use("/tasks", TasksRouter);
 
 server.get('/', (req, res) => {
     res.send('<h1> Its Nice, I like, How Much <h1>')
